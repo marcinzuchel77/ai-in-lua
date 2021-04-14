922533a8bbb685dec867b2329ce95a4c26479844
marcinzuchel77 <82165297+marcinzuchel77@users.noreply.github.com>
2021-04-14 20:15:41 +0200

    Add files via upload

--- first.lua
+++ first.lua
@@ -1,5 +1,8 @@
 os.execute("clear")
 io.write("witaj misiu ... jakie twe imie ? ")
 local a = io.read()
-io.write("witaj ",a,"\n"
-)
+io.write("witaj ",a,"\n")
+io.write("podaj funkcje y=...")
+b = io.read()
+io.write("y=",b," ma miejsca zerowe w ...\n")
+
